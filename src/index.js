0703de23840dd655ed21fd512949a345f2cd1eea
Valeriy Semko <valsemko74@gmail.com>
2022-12-16 16:50:58 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from 'redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/slim-mom-application/">
-      <Provider store={store}>       
+      <Provider>       
         <App />       
       </Provider>
     </BrowserRouter>
